c1703b6d8c1e852bcbdad9ed152a4d0c4ffeb228
Alexander <o.seleznyov@gmail.com>
2022-05-04 14:09:20 +0300

    minor fixes

--- app/js/product-image-slider.js
+++ app/js/product-image-slider.js
@@ -1,16 +1,12 @@
 let slides = document.querySelectorAll('#product_slider .carousel-inner .item img');
 
 slides.forEach(slide => slide.addEventListener('mousemove', setImagePosition));
-//slides.forEach(slide => slide.addEventListener('mouseenter', setImgZoom));
-//slides.forEach(slide => slide.addEventListener('mouseleave', resetImgZoom));
 slides.forEach(slide => slide.addEventListener('mouseleave', setImageImagePosition));
 
 function setImagePosition(e){
-  
   let x = -e.offsetX + 'px';
   let y = -e.offsetY + 'px';
   this.style.transform = `translate(${x}, ${y}) scale(2)`;
-  //console.log(e.offsetX, e.offsetY);  
 }
 
 
