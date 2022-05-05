660e3f19bb0ab5d5c9cdb4d46c3998d03d592b32
Alexander <o.seleznyov@gmail.com>
2022-05-05 13:50:56 +0300

    carousel fixes

--- app/js/product-image-slider.js
+++ app/js/product-image-slider.js
@@ -14,3 +14,15 @@ function setImageImagePosition(e){
   this.style.transform = 'translate(0, 0) scale(1)';
 }
 
+let thumbnails = document.querySelectorAll(".carousel-indicators [data-target='#product_slider']");
+let items = document.querySelectorAll('.carousel-inner .item');
+
+thumbnails.forEach(el => el.addEventListener('mouseenter', setActiveImageItem));
+
+function setActiveImageItem(event){
+  items.forEach(item => item.classList.remove('active'));
+  items[event.target.dataset.slideTo].classList.add('active');
+
+}
+
+
